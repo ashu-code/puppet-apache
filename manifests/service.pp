918701f448afdc203ab1952332322b61f53e986a
root <root@d2ab560dc21c.mylabserver.com>
2022-12-16 00:29:05 +0000

    added a service class

---
+++ manifests/service.pp
@@ -0,0 +1,11 @@
+# Allow the apache service to restart when triggered.
+# @example
+#   include apache::service
+class apache::service {
+  service { "${apache::service_name}":
+    alias      => 'apache_service',
+    ensure     => $apache::service_ensure,
+    enable     => $apache::service_enable,
+    hasrestart => true,
+  }
+}
